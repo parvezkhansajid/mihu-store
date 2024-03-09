4a0d64afc15499925a2b7b3b7bfb32f599209b5b
parvezkhansajid <parvezkhansajid@gmail.com>
2024-03-09 22:25:30 +0600

    Added Search apiFeature

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsynErrors");
+const ApiFeature = require("../utils/apiFeature");
 
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -12,7 +13,8 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const apiFeature = new ApiFeature(Product.find(), req.query).search();
+  const products = await apiFeature.query;
 
   res.status(200).json({
     success: true,
