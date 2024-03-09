67a220b719ad07df1c11d18e93b98546101a2ce7
parvezkhansajid <parvezkhansajid@gmail.com>
2024-03-09 14:53:06 +0600

    Created ProductModel

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,3 +1,14 @@
+const Product = require("../models/productModel");
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
 exports.getAllProducts = (req, res, next) => {
   res.status(200).json({
     message: "Route is working fine!",
